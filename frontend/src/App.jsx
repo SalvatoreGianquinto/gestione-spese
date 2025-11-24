0ddaadf810f7e3b0caa1de8e93a290eeafdd720c
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-11-24 12:06:12 +0100

    modifiche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,37 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom"
 import Register from "./components/Register"
 import Login from "./components/Login"
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 
-function App() {
+function AppWrapper() {
+  const location = useLocation()
+
+  const hideNavbar =
+    location.pathname === "/login" || location.pathname === "/register"
+
   return (
-    <Router>
-      <Navbar />
+    <>
+      {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  )
+}
