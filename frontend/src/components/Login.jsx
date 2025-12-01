77baaa223da566d81abf497ddce137d8ec5afe63
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-12-01 15:47:23 +0100

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError("")
 
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("/login", {
         email,
         password,
       })
