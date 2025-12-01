17dae7a7febb61c809ad55ab509ea70eb640b895
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-12-01 15:15:48 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,17 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const app = express()
-app.use(cors())
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "token"],
+  })
+)
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const authRoutes = require("./routes/auth")
 app.use("/", authRoutes)
@@ -19,10 +28,7 @@ const userRoutes = require("./routes/user")
 app.use("/user", userRoutes)
 
 mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URI)
   .then(() => console.log("Connesso a MongoDB"))
   .catch((err) => console.log("Errore connessione MongoDB:", err))
 
